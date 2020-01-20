DEBUG: Examined if there was any bug related to Wishart mean scaling. There wasn't. Just changes the scaling of samples to scaling od the scale matrix inside the sampling routine. This shouldn't make any difference. Now it is consistent with the entropy calculation method also.

src/garage/np/algos/mod_cem_ssd.py
@@ -128,23 +128,23 @@ class MOD_CEM_SSD(BatchPolopt):
         sample_pd_mat['base'].append({})
         cur_S0_cov = cur_pd_scale['base'][0]['S']
         cur_S0_dof = int(cur_pd_dof['base'][0]['S'])
-        sample_pd_mat['base'][0]['S'] = wishart.rvs(cur_S0_dof, cur_S0_cov) / cur_S0_dof
+        sample_pd_mat['base'][0]['S'] = wishart.rvs(cur_S0_dof, cur_S0_cov/cur_S0_dof)
         cur_D0_cov = cur_pd_scale['base'][0]['D']
         cur_D0_dof = int(cur_pd_dof['base'][0]['D'])
-        sample_pd_mat['base'][0]['D'] = wishart.rvs(cur_D0_dof, cur_D0_cov) / cur_D0_dof
+        sample_pd_mat['base'][0]['D'] = wishart.rvs(cur_D0_dof, cur_D0_cov/cur_D0_dof)
         for k in range(K):
             sample_pd_mat['comp'].append({})
             cur_Sk_cov = cur_pd_scale['comp'][k]['S']
             cur_Sk_dof = int(cur_pd_dof['comp'][k]['S'])
-            sample_pd_mat['comp'][k]['S'] = wishart.rvs(cur_Sk_dof, cur_Sk_cov) / cur_Sk_dof
+            sample_pd_mat['comp'][k]['S'] = wishart.rvs(cur_Sk_dof, cur_Sk_cov/cur_Sk_dof)
             cur_Dk_cov = cur_pd_scale['comp'][k]['D']
             cur_Dk_dof = int(cur_pd_dof['comp'][k]['D'])
-            sample_pd_mat['comp'][k]['D'] = wishart.rvs(cur_Dk_dof, cur_Dk_cov) / cur_Dk_dof
+            sample_pd_mat['comp'][k]['D'] = wishart.rvs(cur_Dk_dof, cur_Dk_cov/cur_Dk_dof)
 
         sample_l = np.zeros(K)
         for k in range(K):
             cur_l_dof_k = int(cur_l_dof[k])
-            sample_l[k] = wishart.rvs(cur_l_dof_k, cur_l_scale[k]) / cur_l_dof_k
+            sample_l[k] = wishart.rvs(cur_l_dof_k, cur_l_scale[k]/cur_l_dof_k)
 
         sample_params = (sample_mu, sample_pd_mat, sample_l)
         return sample_params